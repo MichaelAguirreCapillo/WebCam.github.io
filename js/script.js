8a94a04ce6e3b78e0310eafb2752487b0b459aec
MichaelAguirreCapillo <aguirrem220795@gmail.com>
2022-08-12 16:31:10 -0500

    cambio de variable

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ let btnCambiarCamara = document.querySelector('#btnCambiarCamara');
 let video = document.querySelector('#video');
 let canvas = document.querySelector('#canvas');
 
-let isFaceUser = false;
+var isFaceUser = false;
 var mostrarStream;
 
 btnEncenderCamara.onclick = AbrirCamara;
